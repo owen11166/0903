667ceae343507fb7fc7f05a329e1ca8a037ea507
owen11166 <109325616+owen11166@users.noreply.github.com>
2023-09-03 20:43:09 +0800

    09032043

--- github0903/src/main/java/com/example/demo/controller/HomeController.java
+++ github0903/src/main/java/com/example/demo/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.User;
-import com.example.demo.service.CustomUserDetailsService;
+import com.example.demo.service.EmailService;
 import com.example.demo.service.UserService;
 import com.example.demo.validator.UserValidator;
 
@@ -21,6 +22,9 @@ import com.example.demo.validator.UserValidator;
 public class HomeController {
 	//private CustomUserDetailsService customUserDetailsService;
 	private UserService userService;
+	
+	@Autowired
+	private EmailService emailService;
 	
 	public HomeController(UserService userService) {
 		this.userService = userService;
@@ -64,6 +68,8 @@ public class HomeController {
 			return "register";
 		}
 		userService.save(user);	
+		//需開啟google2階段認證 等到大家都做好在開啟
+		//emailService.sendRegistrationConfirmation(user.getEmail(), user.getUsername());
 	    return "redirect:/registersucessful";
 	}
 
